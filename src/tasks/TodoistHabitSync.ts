7e742ee1c5e82087e85a4abd9c7350d94b3de048
Joseph Hale <me@jhale.dev>
2026-02-27 01:38:53 -0700

    fix: Account for API visibility delays
    Todoist sometimes has a delay between a task's completion
    and that completion's visibility in the API.
    
    If we always save the last sync time as "now", we might miss tasks that were
    just completed but don't yet appear in the API.
    
    So, we grab all tasks completed since a little before the last sync time.
    To avoid double counting, a task will only trigger a habit update if it was
    completed after the last sync time that we have saved.

--- src/tasks/TodoistHabitSync.ts
+++ src/tasks/TodoistHabitSync.ts
@@ -1,17 +1,46 @@
 import { NativeModules } from "react-native";
 import { Storage } from "../lib/Storage";
 import { queryTasks } from "../lib/Todoist"
+import { normalize } from "../lib/normalize";
+import { MINUTES } from "../lib/time";
 
 const { LoopHabitModule } = NativeModules;
 
+/*
+  Todoist sometimes has a delay between a task's completion
+  and that completion's visibility in the API.
+  
+  If we always save the last sync time as "now", we might miss tasks that were
+  just completed but don't yet appear in the API.
+
+  So, we grab all tasks completed since a little before the last sync time.
+  To avoid double counting, a task will only trigger a habit update if it was
+  completed after the last sync time that we have saved.
+ */
+const API_LAG_BUFFER = 30 * MINUTES;
 
 module.exports = async () => {
   const apiToken = Storage.ApiKey.read();
-  const recentlyCompletedTaskIDs = (await queryTasks(apiToken, Storage.LastSync.read())).map(t => t.id);
   const storedTasks = Storage.Tasks.read()
-  const habitTasks = storedTasks.filter(t => t.habit !== undefined).filter(t => recentlyCompletedTaskIDs.includes(t.id))
-  console.log(`Marking ${habitTasks.length} habits as done.`)
+  
+  const queryDate = new Date(Storage.LastSync.read().getTime() - API_LAG_BUFFER);
+  const recentlyCompletedTasks = await queryTasks(apiToken, queryDate);
+  const recentlyCompleted = new Map(recentlyCompletedTasks.map(t => [t.id, t]));
+  const habitCompletingTasks = storedTasks
+    .filter(stored => stored.habit !== undefined)
+    .filter(stored => recentlyCompleted.has(stored.id))
+    .filter(stored => recentlyCompleted.get(stored.id)!.completedAt > stored.completedAt)
+
   // @ts-ignore
-  habitTasks.forEach(async t => await LoopHabitModule.takeHabitAction(t.habit.id, t.habit.action));
+  habitCompletingTasks.forEach(async t => await LoopHabitModule.takeHabitAction(t.habit.id, t.habit.action));
+  console.debug(JSON.stringify({
+    message: 'Sync completed',
+    recentlyCompletedTasks,
+    recentlyCompletedTasksCount: recentlyCompletedTasks.length,
+    habitCompletingTasks,
+    habitCompletingTasksCount: habitCompletingTasks.length,
+  }));
+  
+  Storage.Tasks.write(normalize([...storedTasks, ...recentlyCompletedTasks]));
   Storage.LastSync.write(new Date())
 }
