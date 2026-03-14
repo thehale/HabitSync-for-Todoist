609e5b7602f53e291bfa9c3f1fa4b068a35dd0b5
Joseph Hale <me@jhale.dev>
2026-03-13 22:21:50 -0700

    Remove duplicate code

--- src/ui/ManualSync.tsx
+++ src/ui/ManualSync.tsx
@@ -58,34 +58,3 @@ function SyncDialog({ visible, onDismiss }: SyncDialogProps) {
 const styles = StyleSheet.create({
   row: { flexDirection: 'row', gap: 8 },
 });
-
-interface SyncDialogProps {
-  visible: boolean;
-  onDismiss: () => void;
-}
-function SyncDialog({ visible, onDismiss }: SyncDialogProps) {
-  return (
-    <Dialog visible={visible} onDismiss={onDismiss}
-      title="What is a habit sync?"
-      content={
-        <Text>
-          Every 15 minutes, this app will automatically check Todoist for the
-          day's completed recurring tasks and update your linked habits in Loop
-          Habit Tracker.
-          {'\n\n'}
-          If the sync isn't working, check this app's battery settings to make
-          sure it can run in the background without restrictions.
-          {'\n\n'}
-          You can also manually trigger a sync by pressing the button below.
-        </Text>
-      }
-      actions={
-        <Dialog.Actions>
-          <Button onPress={onDismiss}>Cancel</Button>
-          <Button mode="tonal" onPress={() => Linking.sendIntent("android.settings.IGNORE_BATTERY_OPTIMIZATION_SETTINGS")}>Battery Settings</Button>
-          <Button mode="contained" onPress={() => { runSync(); onDismiss(); }}>Run Sync</Button>
-        </Dialog.Actions>
-      }
-    />
-  );
-}
