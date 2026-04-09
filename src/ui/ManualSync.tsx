d08c0658949357f9a229f776b1d2f7e792b06cef
Joseph Hale <me@jhale.dev>
2026-04-08 17:54:25 -0700

    fix: Inline battery settings
    On narrow screens, the three actions were pushing buttons outside the modal area.
    
    This new approach inlines the battery optimizations button so that it's both closer to its description AND leaves plenty of space for the actions at the bottom of the modal

--- src/ui/ManualSync.tsx
+++ src/ui/ManualSync.tsx
@@ -27,21 +27,26 @@ function SyncDialog({ visible, onDismiss }: SyncDialogProps) {
     <Dialog visible={visible} onDismiss={onDismiss}
       title="What is a habit sync?"
       content={
-        <Text>
-          Every 15 minutes, this app will automatically check Todoist for the
-          day's completed recurring tasks and update your linked habits in Loop
-          Habit Tracker.
-          {'\n\n'}
-          If the sync isn't working, check this app's battery settings to make
-          sure it can run in the background without restrictions.
-          {'\n\n'}
-          You can also manually trigger a sync by pressing the button below.
-        </Text>
+        <>
+          <Text>
+            Every 15 minutes, this app will automatically check Todoist for the
+            day's completed recurring tasks and update your linked habits in Loop
+            Habit Tracker.
+            {'\n\n'}
+            If the sync isn't working, check this app's battery settings to make
+            sure it can run in the background without restrictions.
+            {'\n'}
+          </Text>
+          <Button mode="tonal" onPress={() => Linking.sendIntent("android.settings.IGNORE_BATTERY_OPTIMIZATION_SETTINGS")}>Battery Settings</Button>
+          <Text>
+            {'\n'}
+            You can also manually trigger a sync by pressing the button below.
+          </Text>
+        </>
       }
       actions={
         <Dialog.Actions>
           <Button onPress={onDismiss}>Cancel</Button>
-          <Button mode="tonal" onPress={() => Linking.sendIntent("android.settings.IGNORE_BATTERY_OPTIMIZATION_SETTINGS")}>Battery Settings</Button>
           <Button mode="contained" onPress={() => { runSync(); onDismiss(); }}>Run Sync</Button>
         </Dialog.Actions>
       }
